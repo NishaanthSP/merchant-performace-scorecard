53dd851dec0b0f19aed03a7d81ea928894a8d9ea
NichuSPN <nichunichuspn@gmail.com>
2022-05-18 16:07:30 +0530

    second commit

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,14 +4,14 @@ const {UserDetail} = require('../models/mongoose/user');
 
 const auth_success = (req,res)=>res.json({"msg":"success"});
 // Still in work
-// const remove_user = (req, res) =>{
-//   console.log("Eteres");
-//   UserDetail.remove({"username":req.body.username}, (err) => {
-//     if (err) {
-//       return res.sendStatus(500);
-//     }
-//     return res.sendStatus(200);
-//   })
+const remove_user = (req, res) =>{
+  console.log("Eteres");
+  UserDetail.deleteOne({username: req.body.username}, (err) => {
+    if (err) {
+      return res.sendStatus(500);
+    }
+    return res.sendStatus(200);
+  })
 
-// }
+}
 module.exports = {auth_success, remove_user};
